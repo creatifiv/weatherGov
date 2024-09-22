1a87dca87ec00f8144befe3cd4e427d514a13f45
creatifiv <haywardwilliams@gmail.com>
2024-09-22 15:18:02 -0400

    updated stuff. added bootstrap attribute. default weather now working

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
-var x, x1, x2, x3, latitude, longitude, api, api2, createUlC, capBtns, box2, city, state;
+var x, x1, x2, x3, x4, latitude, longitude, api, api2, createUlC, capBtns, box2, city, state;
+
+/*
+  ===== xReq: Request count
+*/
 
 
-//DefaultLong & Lat: New York City
-longitude = 74.0060;
-latitude = 40.7128;
 
 window.addEventListener('load',() => {
 	x = document.getElementById('response');
@@ -11,10 +12,19 @@ window.addEventListener('load',() => {
 	box2 = document.getElementById('box2');
 	x2 = document.getElementById('state');
 	x3 = document.getElementById('forecast');
+	x4 = document.getElementById('forecastRow');
+
+	loadDefaultWeather();
 
 });
 
+function loadDefaultWeather(){
+		latitude = 40.730610;
+		longitude = -73.9352425;
+		api = "https://api.weather.gov/points/" + latitude + "," + longitude;
 
+		getWeather();
+}
 
 function getUsrLocation(){
 	if ("geolocation" in navigator){
@@ -25,60 +35,57 @@ function getUsrLocation(){
 	}
 }
 
-
 // getUsrLocaion callback
 function successCallback(position){
-
 	latitude = position.coords.latitude;
 	longitude = position.coords.longitude;
 	api = "https://api.weather.gov/points/" + latitude + "," + longitude;
-
 	getWeather();
-
 	console.log("Latitude" + " " + latitude);
 	console.log("Longitude" + " " + longitude);
 }
+
 // getUsrLocation Callback Error
 function errorCallback(error){
 	console.log("Error occured" + error.message);
 }
-
 //Add data to page
 function fillData(){
-
 	x2.innerHTML = city + "," + " "+ state;
+}
+// Count request
+function reqCount(){
+	xReq += 1;
+	console.log(xReq);
 }
 
 
 function createElement(data, days){
-
-	
 	fillData();
 	console.log(data);
 	for(var i = 0; i < days; i++){
 		var mkLi = document.createElement("li");
-		mkLi.innerHTML = data[i].startTime + " " +  " - " + data[i].endTime + "<br/> " + data[i].name + " " +  data[i].shortForecast +  "<br/>" + data[i].temperature + " " + data[i].temperatureUnit + "<br/>";
-		x3.appendChild(mkLi);
+		mkLi.className = "col-sm-1";
+		mkLi.innerHTML = data[i].name + " " +  data[i].shortForecast +  "<br/>" + data[i].temperature + " " + data[i].temperatureUnit + "<br/>";
+		x4.appendChild(mkLi);
 		console.log(days);
 	}
 }
+
+/* ***** FIX: createUsCapitalsButtons Needs to go. Ul is in index.html, no need to create **** */
 
 function createUsCapitalsButtons(capitals){
 	createUlC = document.createElement('ul');
 	box2.appendChild(createUlC);
 	console.log(capitals);
-
 	for(var i = 0; i < capitals.length; i++){
-
 		createUlC.appendChild(createUlC);
 		createUlC.innerHTML = "";
-
 	}
 }
 
 
 function getUsCap(){
-
 	fetch("./us_capitals.json", {
 		method: 'GET'
 	})
@@ -102,7 +109,6 @@ function getUsCap(){
 
 
 function getWeather(){
-	
 	fetch(api, {
 		method:'GET',
 		headers: {
