10c6d331ad378ff5b2286e6ede79bb4b1451e102
Hayward <haywardwilliams@gmail.com>
2024-11-14 14:04:36 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,12 +1,16 @@
 window.onerror = function(message, source, lineno, colno, error) {
-  var errorMessage = "Error caught in window.onerror:"
-  errorMessage += `Message: ${message}`;
-  errorMessage += `Source: ${source}`; 
-  errorMessage += `Line: ${lineno}`;
-  errorMessage += `Column: ${colno}`;
-  errorMessage += `Error object: ${error}`:
-  try{alert(errorMessage);}
-catch(error){ alert(error.message)}
-  return true; // Suppress the default error handling
-};
+  var errorMessage = "Error caught in window.onerror: ";
+  errorMessage += `Message: ${message} `;
+  errorMessage += `Source: ${source} `;
+  errorMessage += `Line: ${lineno} `;
+  errorMessage += `Column: ${colno} `;
+  errorMessage += `Error object: ${error}`;
 
+  try {
+    alert(errorMessage);  // Display error message in an alert
+  } catch (error) {
+    alert("Error occurred while trying to show the error message: " + error.message);
+  }
+
+  return true;  // Suppress the default error handling
+};
