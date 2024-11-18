d8f4515f105f32004ee5d92a00bf6cb9aad9ea9e
Hayward <haywardwilliams@gmail.com>
2024-11-18 14:29:37 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -76,7 +76,7 @@ function createElement(data, days){
 	for(var i = 0; i < days; i++){
 		var mkLi = document.createElement("li");
 		mkLi.className = "col-sm-1";
-		mkLi.innerHTML = i + "." + " " +  data[i].name + " " +  data[i].shortForecast +  "<br/>" + data[i].temperature + " " + data[i].temperatureUnit + "<br/>";
+		mkLi.innerHTML = data[i].name + " " +  data[i].shortForecast +  "<br/>" + data[i].temperature + " " + data[i].temperatureUnit + "<br/>";
 		x4.appendChild(mkLi);
 		console.log(days);
 	}
