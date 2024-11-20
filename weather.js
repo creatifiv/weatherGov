890b4ee192a9a43c0ba912ee01711831ac239776
Hayward <haywardwilliams@gmail.com>
2024-11-20 05:27:49 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -80,7 +80,7 @@ function createElement(data, days){
 	for(var i = 0; i < days; i++){
 		var mkLi = document.createElement("li");
 		var createLi2 = document.createElement("li");
-		mkLi.className = "col-sm-1" + " " + "weather-${dayCount}"; // ADDING A CLASS TO CREATE A CHECK TO GROUP THE DATA OF MORNING AND NIGHT FORECAST TOGETHER
+		mkLi.className = "col-sm-1" + " " + "weather-${dayCount}" + " " + "weather-box"; // ADDING A CLASS TO CREATE A CHECK TO GROUP THE DATA OF MORNING AND NIGHT FORECAST TOGETHER
 		mkLi.innerHTML = data[i].name + " " +  data[i].shortForecast +  "<br/>" + data[i].temperature + " " + data[i].temperatureUnit + "<br/>";
 		x4.appendChild(mkLi);
 		createLi2.innerHTML = data[i].name;
