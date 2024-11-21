f81991708e3ea4cca3e63ea6bf2246dda87d0e60
Hayward <haywardwilliams@gmail.com>
2024-11-21 11:25:29 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-var x, x1, x2, x3, x4, x5, x6, latitude, longitude, api, api2, createUlC, capBtns, box2, city, state, dayCount;
+var x, x1, x2, x3, x4, x5, x6, latitude, longitude, api, api2, createUlC, capBtns, box2, city, state, dayCount, mkLi;
 
 /*
   ===== xReq: Request count
