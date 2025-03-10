aaba45ccd4d2ce2a8d7febb90c91d511d4a9cf26
Hayward <haywardwilliams@gmail.com>
2025-03-10 15:15:34 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -297,7 +297,7 @@ function createElement(data, days){
 function parseInput(array, input){
 		var pattern = `\\b${input}\\}`;
   regex = new RegExp(pattern, 'gi'); // 'i' flag for case-insensitive search
-		x9.innerHTML = regex.test(usCapArray[0].name);
+		x9.innerHTML = regex.test("a");
 		for(var i = 0; i < usCapArray.length; i++){
 					if(regex.test(usCapArray[i].name) == true){
 							x9.innerHTML += usCapArray[i].name;
