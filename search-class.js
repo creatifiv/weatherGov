4ebfc472bcb0c48028228883f397a8b446917448
Hayward <haywardwilliams@gmail.com>
2025-03-17 14:22:25 -0400

    Update search-class.js
    updated trie

--- search-class.js
+++ search-class.js
@@ -1,43 +1,42 @@
 // TrieNode
-
 export class TrieNode {
-			constructor(){
-					this.children = {};
-					this.isEndOfWord = false;
-			}
-	}
-	export class Trie {
-			constructor(){
-				this.root = new TrieNode();
-				console.log("New Trie planted");
-				this.trieAlert = "Enter city, stae, or zip";
-        this.indexLog = "new Tree planted";
-			}
-			insert(word){
-				let node = this.root;
-        word = word.toLowerCase();
-			for (let char of word) {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+export class Trie {
+  constructor() {
+    this.root = new TrieNode();
+    console.log("New Trie planted");
+    this.trieAlert = "Enter city, state, or zip";
+    this.indexLog = "new Tree planted";
+  }
+
+  // Insert a word into the Trie
+  insert(word) {
+    let node = this.root;
+    word = word.toLowerCase();
+    for (let char of word) {
       if (!node.children[char]) {
         node.children[char] = new TrieNode(); // Create a new node if char not found
-      }//end if
+      }
       node = node.children[char]; // Move to the next node
-      //dropDownBox[0].innerHTML = node;
-     // console.log("char" + " " + char);
-    }//end for
+    }
     node.isEndOfWord = true; // Mark the end of a valid word
-	}//end insert
+  }
 
-	// Search for a word in the trie
+  // Search for a word in the trie
   search(word) {
     let node = this.root;
-    for (let char of word) {
-      if (!node.children[char]){
-      	console.log(word + " " + "Not found");
-      	 this.trieAlert = word + " " + "not found";
+    for (let char of word.toLowerCase()) {
+      if (!node.children[char]) {
+        console.log(word + " " + "Not found");
+        this.trieAlert = word + " " + "not found";
         return false; // Word not found
       }
       node = node.children[char];
-						this.trieAlert += node.children[char];
     }
     console.log(node.isEndOfWord);
     return node.isEndOfWord; // Returns true if it's a complete word
@@ -49,29 +48,32 @@ export class TrieNode {
     prefix = prefix.toLowerCase();
     for (let char of prefix) {
       if (!node.children[char]) {
-      	 console.log("Prefix:" + " " + prefix + " " + "does not exist");
-         this.trieAlert = "not found";
+        console.log("Prefix:" + " " + prefix + " " + "does not exist");
+        this.trieAlert = "Prefix not found";
         return false;
       }
       node = node.children[char];
-      console.log(prefix + " " + "exist");
+      console.log(prefix + " " + "exists");
     }
     return true; // Prefix exists
   }
 
+  // Collect words starting from a given node
   collectWords(node, prefix, words) {
     if (node.isEndOfWord) words.push(prefix); // If it's the end of a word, add it to the result
 
     for (let char in node.children) {
-      collectWords(node.children[char], prefix + char, words); // Recurse and build the word
+      this.collectWords(node.children[char], prefix + char, words); // Recurse and build the word
     }
   }
 
+  // Get all words in the trie
   getAllWords() {
     let words = [];
-    collectWords(root, "", words); // Start from root and an empty prefix
+    this.collectWords(this.root, "", words); // Start from root and an empty prefix
     return words;
   }
-
 }
-var newTrie = new Trie();
+
+
+
