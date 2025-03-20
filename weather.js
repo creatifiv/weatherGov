11f734cffaa3ddaec8f713bfe36bf57e62582b50
Hayward <haywardwilliams@gmail.com>
2025-03-20 11:52:21 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -22,7 +22,7 @@ amPmBoxes = [document.getElementById('d1-am'), document.getElementById('d1-pm'),
 		searchOutput.innerHTML = event.target.value;
 		text = event.target.value;
 		newTrie.trieAlert = text;
-		dropDownBox[0].innerHTML += newTrie.wordAlert();
+		dropDownBox[0].innerHTML += newTrie.wordAlert;
 		//dropDownBox[0].innerHTML = newTrie.trieAlert;
 		//parseInput(text);
 		console.log("text var:" + " " +  text);
