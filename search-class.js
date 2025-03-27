87d9eaf30d11a0f6180ca49e774561edef4eab9f
Hayward <haywardwilliams@gmail.com>
2025-03-27 13:07:10 -0400

    Update search-class.js

--- search-class.js
+++ search-class.js
@@ -61,7 +61,7 @@ export class Trie {
   wordStartsWith(letter){
     var words = " ";
     for(var char in this.root.children){
-      words += this.root.children[char];
+      words += this.root.children[char].toString();
     }
     alert(words);
     return words;
