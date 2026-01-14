f0cb4ec321171452f1c590a2e229741b362133f1
Hayward <haywardwilliams@gmail.com>
2026-01-13 21:01:16 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -333,7 +333,7 @@ function getWeather(){
 	})
 	.then(data => {
 		var newData = data.properties.periods;
-		newShortForecast = data;
+		newShortForecast = data.properties;
 					x6.innerHTML = newShortForecast;
 		createElement(newData, newData.length);
 		x += JSON.stringify(newData, null, 2);
