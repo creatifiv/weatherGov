b7e98c5179bc40647f7456b8892df2e0ecabcfc5
Hayward <haywardwilliams@gmail.com>
2026-02-03 11:46:04 -0500

    Create trie.js
    Initial file push

---
+++ trie.js
@@ -0,0 +1,15 @@
+export class TrieNode {
+		constructor(){
+            this.children = {};
+            this.isEndOfWord = false;
+		}
+	}
+    
+export class Trie {
+	constructor(){
+        this.root = new TrieNode();
+        console.log("New Trie planted");
+        }
+    }
+    
+    export default Trie
