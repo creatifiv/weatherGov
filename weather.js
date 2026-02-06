4f682fa8412409d3ee39f80d723b57a91f946a73
Hayward <haywardwilliams@gmail.com>
2026-02-06 05:32:55 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ var x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, regex, shortForecast, usCap
 import {TrieNode, Trie} from './trie.js';
 
 var trie = new Trie();  
-var trieNode = new TrieNode();
+
 
 
 
