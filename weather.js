077b9b4c4d61cbecb2a6ce625131af0133471f47
Hayward <haywardwilliams@gmail.com>
2026-02-06 07:21:39 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-var x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, regex, shortForecast, usCapArray,getUserLoBtn, dropDownBox, locationOutput, jsonData, text, searchForm, searchClicked, amPmBoxes, searchOutput, searchSubmit, search, weatherIcon, dayOfWeek, latitude, longitude, api, forecastApi, createUlC, capBtns, box2, city, state, dayCount, mkLi, day1, day2, day3, day4, day5, day6, day7, cityData, dataOutput;
+var x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, regex, shortForecast, usCapArray,getUserLoBtn, dropDownBox, locationOutput, jsonData, text, searchForm, searchClicked, amPmBoxes, searchOutput, searchSubmit, search, weatherIcon, dayOfWeek, latitude, longitude, api, forecastApi, createUlC, capBtns, box2, city, state, dayCount, mkLi, day1, day2, day3, day4, day5, day6, day7, cityData;
 
 import {TrieNode, Trie} from './trie.js';
 
@@ -43,7 +43,7 @@ window.addEventListener('load',() => {
 	dropDownBox = document.getElementsByClassName("dropdown-item");
 	locationOutput = document.getElementById("locationOutput");
 	getUserLoBtn = document.getElementById("getUserLocation");
-	dataOutput = document.getElementById("data-output");
+	//dataOutput = document.getElementById("data-output");
 
 	amPmBoxes = [
 		document.getElementById('d1-am'),
