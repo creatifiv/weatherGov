9273076510773909e60bbd9528b9ac5c3ac30ba5
Hayward <haywardwilliams@gmail.com>
2026-02-07 14:14:58 -0500

    Update trie.js
    Working on dfs

--- trie.js
+++ trie.js
@@ -9,10 +9,9 @@ export class Trie {
 	constructor(){
         this.root = new TrieNode();
         console.log("New Trie planted");
-        }
+    }
       insert(word){
          let node = this.root;
-         
          for (const ch of word){
             if(!node.children[ch]){
                node.children[ch] = new TrieNode();
@@ -21,6 +20,23 @@ export class Trie {
          }
          node.isEndOfWord = true;
       }
+    
+    getWordsWithPrefix(prefix){
+        let node = this.root;
+        for(char of prefix){
+            if(!node.children[char]){
+                return [];
+            }
+            node = node.children[char];
+        }
+        const results = [];
+        const dfs = (currentNode, path) => {
+            if(currentNode.isEndOfWord){
+                results.push(prefix + path);
+            }
+        }// End DFS
+        
     }
+}
     
     
