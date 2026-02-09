40c941d0bfe13fe4ed351ae5941c065f298021cd
Hayward <haywardwilliams@gmail.com>
2026-02-09 14:32:25 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -97,7 +97,7 @@ function loadDefaultWeather(){
 			// searchRes = presfix search results 
 				var preX =	trie.searchPrefix(event.target.value);
 				for(var i = 0; i < preX.length; i++){
-						searchPreOut.innerHTML += "searchPreOutput:" + " " + preX[i];
+						searchPreOut.innerHTML += preX[i] <br>;
 				}
 		}
 		
